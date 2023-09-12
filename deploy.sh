d770ebc69f747094a8db463a8b8c20349bf7fbf5
张景赫 <venusxk6214@foxmail.com>
2023-09-12 21:32:51 +0800

    update css link from output to output_main & output_vitepress

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ mkdir /home/www/deploy.zjh.asia/blog;
 mkdir /home/www/deploy.zjh.asia/academic;
 cd hugo && hugo && cp -rf public/* /home/www/deploy.zjh.asia/blog/ && rm -rf public;
 cd ../vitepress/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.zjh.asia/academic/ && rm -rf docs/.vitepress/dist/*;
-cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp output.css /home/www/deploy.zjh.asia/;
+cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp output_main.css /home/www/deploy.zjh.asia/;
