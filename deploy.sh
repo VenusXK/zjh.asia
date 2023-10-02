03b100db2c40d80f6cbe00c481966e50e81f7fc8
张景赫 <venusxk6214@foxmail.com>
2023-10-02 23:53:20 +0800

    update content 20231002

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ mkdir /home/www/deploy.zjh.asia/academic;
 cd hugo && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/life/ && rm -rf public;
 cd ../hugo2 && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/tech/ && rm -rf public;
 cd ../vitepress/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.zjh.asia/academic/ && rm -rf docs/.vitepress/dist/*;
-cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp contact.html /home/www/deploy.zjh.asia/ && cp index_en.html /home/www/deploy.zjh.asia/  && cp output_main.css /home/www/deploy.zjh.asia/ && cp 87a96bae841a1174350381c46d22849c.txt /home/www/deploy.zjh.asia/;
+cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp wechat.html /home/www/deploy.zjh.asia/ && cp index_en.html /home/www/deploy.zjh.asia/  && cp output_main.css /home/www/deploy.zjh.asia/ && cp 87a96bae841a1174350381c46d22849c.txt /home/www/deploy.zjh.asia/;
 # cd ../email/ && cp * /home/www/deploy.zjh.asia/email/;
