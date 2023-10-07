8510f145c394b1b8f74dcdf7f66fe5f5eb31a3ac
张景赫 <venusxk6214@foxmail.com>
2023-10-07 17:09:25 +0800

    upload deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ mkdir /home/www/deploy.zjh.asia/artworks;
 cd hugo && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/life/ && rm -rf public;
 cd ../hugo2 && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/tech/ && rm -rf public;
 cd ../artworks && npm install && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/artworks/ && rm -rf public;
+cp -rf bugfix/* /home/www/deploy.zjh.asia/artworks/;
 cd ../vitepress/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.zjh.asia/academic/ && rm -rf docs/.vitepress/dist/*;
 cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp wechat.html /home/www/deploy.zjh.asia/ && cp index_en.html /home/www/deploy.zjh.asia/  && cp output_main.css /home/www/deploy.zjh.asia/ && cp 87a96bae841a1174350381c46d22849c.txt /home/www/deploy.zjh.asia/;
 # cd ../email/ && cp * /home/www/deploy.zjh.asia/email/;
