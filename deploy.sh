6f50a32d2cce684a733df7ace53db1d70ec70c83
张景赫 <venusxk6214@foxmail.com>
2023-10-12 15:37:44 +0800

    recode the tech page & rm the academic page

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cd hugo && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/life/
 cd ../artworks && npm install && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/artworks/ && rm -rf public;
 cp -rf bugfix/* /home/www/deploy.zjh.asia/artworks/; # 修复了npx没有对tailwindcss重新统计的bug
 cd ../vitepress/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.zjh.asia/tech/ && rm -rf docs/.vitepress/dist/*;
-cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp wechat.html /home/www/deploy.zjh.asia/ && cp index_en.html /home/www/deploy.zjh.asia/  && cp output_main.css /home/www/deploy.zjh.asia/ && cp 87a96bae841a1174350381c46d22849c.txt /home/www/deploy.zjh.asia/;
+cd ../main/ && cp index.html /home/www/deploy.zjh.asia/ && cp wechat.html /home/www/deploy.zjh.asia/ && cp output_main.css /home/www/deploy.zjh.asia/ && cp 87a96bae841a1174350381c46d22849c.txt /home/www/deploy.zjh.asia/;
