dd313cec63fa00e7361cbc0fcd52db80a9518dc7
张景赫 <venusxk6214@foxmail.com>
2023-11-22 21:15:37 +0800

    switch to blog.zjh.asia

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-rm -rf /home/www/deploy.zjh.asia;
-mkdir /home/www/deploy.zjh.asia;
-mkdir /home/www/deploy.zjh.asia/life;
+rm -rf /home/www/deploy.blog.zjh.asia;
+mkdir /home/www/deploy.blog.zjh.asia;
+mkdir /home/www/deploy.blog.zjh.asia/life;
 
-cd life && hugo --buildFuture && cp -rf public/* /home/www/deploy.zjh.asia/life/ && rm -rf public;
-cd ../tech/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.zjh.asia && rm -rf docs/.vitepress/dist/*;
+cd life && hugo --buildFuture && cp -rf public/* /home/www/deploy.blog.zjh.asia/life/ && rm -rf public;
+cd ../tech/ && yarn run docs:build && cp -rf docs/.vitepress/dist/* /home/www/deploy.blog.zjh.asia && rm -rf docs/.vitepress/dist/*;
