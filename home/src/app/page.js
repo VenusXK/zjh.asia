b4353678141be261b4de7017922fa721eb998ca4
张景赫 <venusxk6214@foxmail.com>
2023-12-05 10:36:40 +0800

    update content 20231205

--- home/src/app/page.js
+++ home/src/app/page.js
@@ -1,16 +1,18 @@
 'use client'
 import { Liu_Jian_Mao_Cao } from 'next/font/google'
+import TheMenu from './menu';
+
 const ljmc = Liu_Jian_Mao_Cao({ weight: ['400'],subsets: ['latin'] })
+
 
 export default function Home() {
 
   return (
     <>
-      <div className='jupyter'>
-        <a href='http://zjh.asia:8888' target='_blank'>
-          <img src='https://mysite-bucket.oss-cn-wulanchabu.aliyuncs.com/language_logo/jupyter-seeklogo.com.svg'></img>
-        </a>
+      <div className='theMenu'>
+        <TheMenu></TheMenu>
       </div>
+
       <div className={"container " + ljmc.className}>
         <ul>
           <li><div className="goto"><a href="https://zjh.asia/notebook" target="_blank">笔记</a></div></li>
