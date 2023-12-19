922f9d7e946a9a906c4e926d75353331c2ae5b4e
张景赫 <venusxk6214@foxmail.com>
2023-12-19 13:59:53 +0800

    update content 20231219

--- home/src/app/page.js
+++ home/src/app/page.js
@@ -1,16 +1,21 @@
 'use client'
 import TheMenu from './menu';
 
-const text = "暂时 的 得意 和 挫折 从 人生 长度 来看 并不 重要 开心 才是 最重要的 :-)"
-const text_split = text.split(' ');
+const text = "暂时的得意和挫折|从人生长度来看|并不重要|开心|才最重要 :-)"
+const text_split = text.split('|');
 
 function ShadeEachChar(){
   const shade = text_split.map((char, index) => {
-    if(char === "开心"){
-      return <span key={index} className='delay-500 duration-200 ease-in hover:text-white hover:bg-purple-500 dark:hover:bg-amber-500 z-10 hover:cursor-default'>开心</span>
+    if(char.at(0) === "开"){
+      return <>
+      <span key={index} className='duration-100 ease-in hover:text-white hover:bg-purple-500 dark:hover:bg-amber-500 z-10 hover:cursor-default'>
+        <span className='underline decoration-wavy'>开心</span>
+        {char.slice(2)}</span>
+      </>
+      
     }
     else{
-      return <span key={index} className='delay-500 duration-1000 hover:text-black dark:hover:text-white hover:cursor-default'>{char}</span>
+      return <span key={index} className='duration-500 hover:text-stone-500 dark:hover:text-white hover:cursor-default'>{char}</span>
     }
   })
   return <div>{shade}</div>
@@ -24,7 +29,8 @@ export default function Home() {
       <div className='theMenu'>
         <TheMenu></TheMenu>
       </div>
-      <div className=' text-white/0 dark:text-white/0 m-auto w-full text-left text-9xl sm:text-8xl '>
+      {/*  */}
+      <div className='m-auto text-stone-200 dark:text-black  w-full text-left text-9xl sm:text-8xl '>
           <ShadeEachChar />
       </div>
     </div>
